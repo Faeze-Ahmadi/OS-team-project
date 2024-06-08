985d447bc6ec61614231846718869de7307f3ca9
Faeze <faezeahmadi7173@gmail.com>
2024-06-08 12:59:08 +0330

    faeze/save_metadata

--- Unsplash.py
+++ Unsplash.py
@@ -21,3 +21,14 @@ def download_image(url, save_path):
     response = requests.get(url)
     with open(save_path, 'wb') as file:
         file.write(response.content)
+
+def save_metadata(image_id, photographer, category, save_path):
+    metadata = {
+        'image_id': image_id,
+        'photographer': photographer,
+        'category': category
+    }
+    with open(save_path, 'w') as file:
+        json.dump(metadata, file, indent=4)
+
+image_links = get_image_links()
